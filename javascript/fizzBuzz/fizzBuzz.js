8c5eb8664b00004ab7be0bff95a95326b22a86d8
Pedro <pedrogdsilva410@gmail.com>
2024-09-07 09:50:26 +0200

    Update fizzBuzz.js

--- javascript/fizzBuzz/fizzBuzz.js
+++ javascript/fizzBuzz/fizzBuzz.js
@@ -1,17 +1,19 @@
 const output = [];
-let nextNumber = 1;
+let count = 1;
 
 function fizzBuzz() {
-  if (nextNumber % 3 === 0 && nextNumber % 5 === 0) {
-    output.push("FizzBuzz");
-  } else if (nextNumber % 3 === 0) {
-    output.push("Fizz");
-  } else if (nextNumber % 5 === 0) {
-    output.push("Buzz");
-  } else {
-    output.push(nextNumber);
-  }
+  while (count <= 100) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+      output.push("Fizz");
+    } else if (count % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(count);
+    }
 
-  nextNumber++;
+    count++;
+  }
   console.log(output);
 }
