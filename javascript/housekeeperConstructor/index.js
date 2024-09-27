1b0f647a753d545a2ce49b6b9d4d388f34f17fae
Pedro <pedrogdsilva410@gmail.com>
2024-09-27 22:25:07 +0200

    Update index.js

--- javascript/housekeeperConstructor/index.js
+++ javascript/housekeeperConstructor/index.js
@@ -2,6 +2,10 @@ function Housekeeper(name, age, experience) {
   this.name = name;
   this.age = age;
   this.experience = experience;
+  this.clean = function () {
+    alert("Cleaning in progress");
+    //cleaning is done
+  };
 }
 
 const housekeeper1 = new Housekeeper("Vanessa", 19, 2);
