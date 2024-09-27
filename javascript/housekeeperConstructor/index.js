64b515b836f7d532d472a4f3cf366c49117f07ea
Pedro <pedrogdsilva410@gmail.com>
2024-09-27 22:20:19 +0200

    Create index.js

---
+++ javascript/housekeeperConstructor/index.js
@@ -0,0 +1,9 @@
+function Housekeeper(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+const housekeeper1 = new Housekeeper("Vanessa", 19, 2);
+
+console.log(housekeeper1);
